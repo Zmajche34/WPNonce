e679db6684dbed21df31de54dea052a018de5647
Marko Novakovic <mnovakovic@westum.com>
2017-08-28 11:30:55 +0200

    Improved tests

--- tests/Zmajche34/WPNonce/WPNonceTest.php
+++ tests/Zmajche34/WPNonce/WPNonceTest.php
@@ -53,6 +53,33 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 		Monkey::setUpWP();
 		$this->action   = 'action';
 		$this->request_name  = 'request';
+
+		// wp_create_nonce is mocked with sha1
+		Functions::when( 'wp_create_nonce' )->alias( 'sha1' );
+		// wp_nonce_field is mocked by this function
+		Functions::expect( 'wp_nonce_field' )->andReturnUsing( function ( $action, $request_name, $referer, $echo ) {
+			$string = $action . $request_name;
+			if ( $referer ) {
+				$string .= 'referer';
+			}
+			return $string;
+		} );
+		// wp_nonce_url is mocked by this function
+		Functions::expect( 'wp_nonce_url' )->andReturnUsing( function ( $url, $action, $request_name ) {
+			return $url . $action . $request_name;
+		} );
+		// wp_verify_nonce is mocked by this function
+		Functions::expect( 'wp_verify_nonce' )->andReturnUsing( function ( $nonce, $action ) {
+			return sha1( $action ) === $nonce ? 1 : false;
+		} );
+		// mock wp_unslash is mocked by this function
+		Functions::expect( 'wp_unslash' )->andReturnUsing( function ( $string ) {
+			return $string;
+		} );
+		// sanitize_text_field is mocked by this function
+		Functions::expect( 'sanitize_text_field' )->andReturnUsing( function ( $string ) {
+			return $string;
+		} );
 	}
 
 	/**
@@ -61,6 +88,22 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 	public function tearDown() {
 		Monkey::tearDownWP();
 		parent::tearDown();
+	}
+
+	/**
+	 * Check if WPNonce initializzes the data correctly, when lifetime is not set.
+	 **/
+	public function testConstructorWithoutLifetime() {
+		$this->lifetime = null;
+		Filters::expectAdded( 'nonce_life' )->never();
+		$this->nonce_object = new WPNonce(
+			$this->action,
+			$this->request_name,
+			$this->lifetime
+		);
+		self::assertSame( $this->nonce_object->get_action(), $this->action );
+		self::assertSame( $this->nonce_object->get_request_name(), $this->request_name );
+		self::assertSame( $this->nonce_object->get_lifetime(), DAY_IN_SECONDS );
 	}
 
 	/**
@@ -77,29 +120,13 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 
 		self::assertSame( $this->nonce_object->get_action(), $this->action );
 		self::assertSame( $this->nonce_object->get_request_name(), $this->request_name );
-		self::assertSame( $this->nonce_object->get_lifetime(), $this->lifetime );
-		self::assertSame( $this->nonce_object->get_lifetime( DAY_IN_SECONDS ), $this->lifetime );
-	}
-
-	/**
-	 * Check if WPNonce initializzes the data correctly, when lifetime is not set.
-	 **/
-	public function testConstructorWithoutLifetime() {
-		$this->lifetime = null;
-		Filters::expectAdded( 'nonce_life' )->never();
-		$this->nonce_object = new WPNonce(
-			$this->action,
-			$this->request_name,
-			$this->lifetime
-		);
+		self::assertSame( $this->nonce_object->get_lifetime(), DAY_IN_SECONDS );
 	}
 
 	/**
 	 * Check the create function.
 	 */
 	public function testCreate() {
-		// wp_create_nonce is mocked with sha1
-		Functions::when( 'wp_create_nonce' )->alias( 'sha1' );
 		$this->lifetime = DAY_IN_SECONDS;
 		$this->nonce_object = new WPNonce(
 			$this->action,
@@ -107,25 +134,13 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 			$this->lifetime
 		);
 		$nonce = $this->nonce_object->create();
-
 		self::assertSame( $nonce, $this->nonce_object->get_nonce() );
 	}
 
 	/**
-	 * Test the create field function.
+	 * Test the create field function without referer.
 	 */
-	public function testCreateField() {
-		// wp_create_nonce is mocked with sha1
-		Functions::when( 'wp_create_nonce' )->alias( 'sha1' );
-		// wp_nonce_field is mocked by this function
-		Functions::expect( 'wp_nonce_field' )->andReturnUsing( function ( $action, $request_name, $referer, $echo ) {
-			$string = $action . $request_name;
-			if ( $referer ) {
-				$string .= 'referer';
-			}
-			return $string;
-		} );
-
+	public function testCreateFieldWithoutReferer() {
 		$this->lifetime = DAY_IN_SECONDS;
 		$this->nonce_object = new WPNonce(
 			$this->action,
@@ -134,6 +149,18 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 		);
 		$field = $this->nonce_object->create_field();
 		self::assertSame( $field, $this->action . $this->request_name );
+	}
+
+	/**
+	 * Test the create field function with referer.
+	 */
+	public function testCreateFieldWithReferer() {
+		$this->lifetime = DAY_IN_SECONDS;
+		$this->nonce_object = new WPNonce(
+			$this->action,
+			$this->request_name,
+			$this->lifetime
+		);
 		$field = $this->nonce_object->create_field( true );
 		self::assertSame( $field, $this->action . $this->request_name . 'referer' );
 	}
@@ -142,13 +169,6 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 	 * Test the create URL function.
 	 */
 	public function testCreateURL() {
-		// wp_create_nonce is mocked with sha1
-		Functions::when( 'wp_create_nonce' )->alias( 'sha1' );
-		// wp_nonce_url is mocked by this function
-		Functions::expect( 'wp_nonce_url' )->andReturnUsing( function ( $url, $action, $request_name ) {
-			return $url . $action . $request_name;
-		} );
-
 		$this->lifetime = DAY_IN_SECONDS;
 		$this->nonce_object = new WPNonce(
 			$this->action,
@@ -162,24 +182,9 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	/**
-	 * Check the verify function.
+	 * Check when the verify function is valid.
 	 */
-	public function testVerification() {
-		// wp_create_nonce is mocked with sha1
-		Functions::when( 'wp_create_nonce' )->alias( 'sha1' );
-		// wp_verify_nonce is mocked by this function
-		Functions::expect( 'wp_verify_nonce' )->andReturnUsing( function ( $nonce, $action ) {
-			return sha1( $action ) === $nonce ? 1 : false;
-		} );
-		// mock wp_unslash is mocked by this function
-		Functions::expect( 'wp_unslash' )->andReturnUsing( function ( $string ) {
-			return $string;
-		} );
-		// sanitize_text_field is mocked by this function
-		Functions::expect( 'sanitize_text_field' )->andReturnUsing( function ( $string ) {
-			return $string;
-		} );
-
+	public function testVerificationValid() {
 		$this->lifetime = DAY_IN_SECONDS;
 		$this->nonce_object = new WPNonce(
 			$this->action,
@@ -189,12 +194,34 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 		$nonce = $this->nonce_object->create();
 		$valid = $this->nonce_object->verify( $nonce );
 		self::assertTrue( $valid );
+	}
 
-		// Check if nonce is not valid.
-		$not_valid = $this->nonce_object->verify( 'not-valid' . $nonce );
+	/**
+	 * Check when the verify function is invalid.
+	 */
+	public function testVerificationInvalid() {
+		$this->lifetime = DAY_IN_SECONDS;
+		$this->nonce_object = new WPNonce(
+			$this->action,
+			$this->request_name,
+			$this->lifetime
+		);
+		$nonce = $this->nonce_object->create();
+		$not_valid = $this->nonce_object->verify( 'invalid' . $nonce );
 		self::assertFalse( $not_valid );
+	}
 
-		// Check auto-nonce assignment.
+	/**
+	 * Check when the verify function is assigned from the request.
+	 */
+	public function testVerificationAutoAssigned() {
+		$this->lifetime = DAY_IN_SECONDS;
+		$this->nonce_object = new WPNonce(
+			$this->action,
+			$this->request_name,
+			$this->lifetime
+		);
+		$nonce = $this->nonce_object->create();
 		$_REQUEST[ $this->request_name ] = $nonce;
 		$this->nonce_object = new WPNonce(
 			$this->action,
@@ -209,21 +236,6 @@ class WPNonceTest extends \PHPUnit_Framework_TestCase {
 	 * Check the get nonce age function.
 	 **/
 	public function testGetNonceAge() {
-		// wp_create_nonce is mocked with sha1
-		Functions::when( 'wp_create_nonce' )->alias( 'sha1' );
-		// wp_verify_nonce is mocked by this function
-		Functions::expect( 'wp_verify_nonce' )->andReturnUsing( function ( $nonce, $action ) {
-			return sha1( $action ) === $nonce ? 1 : false;
-		} );
-		// mock wp_unslash is mocked by this function
-		Functions::expect( 'wp_unslash' )->andReturnUsing( function ( $string ) {
-			return $string;
-		} );
-		// sanitize_text_field is mocked by this function
-		Functions::expect( 'sanitize_text_field' )->andReturnUsing( function ( $string ) {
-			return $string;
-		} );
-
 		$this->lifetime = DAY_IN_SECONDS;
 		$this->nonce_object = new WPNonce(
 			$this->action,
